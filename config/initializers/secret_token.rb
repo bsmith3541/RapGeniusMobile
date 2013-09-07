6e1e7bf5aead5ba25ae2e16d45781804d025e04e
Brandon Smith <brandon.smith@yale.edu>
2013-09-07 10:51:49 -0400

    added rapgenius gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RapgeniusBackend::Application.config.secret_key_base = 'd058c7946a8ab9bc63aa389b3bb3396b8133d520647743b5552f4dec671f1f8b263c27449e48dc746d2febdb10db1e2050b908260411ef65be0d1a36f5c6b103'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RapgeniusBackend::Application.config.session_store :encrypted_cookie_store, key: '_rapgenius-backend_session'
