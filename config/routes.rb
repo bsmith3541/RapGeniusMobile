9385ad35d653e736f85b9fab1bc05f9136056f27
Brandon Smith <brandon.smith@yale.edu>
2013-09-07 12:10:06 -0400

    added custom route template

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ RapgeniusBackend::Application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+  get 'getlyrics/' => 'controllername#methodname'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
